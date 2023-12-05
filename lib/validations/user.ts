48803de3b5a169625dfc876746b28e25040520b6
asaf29999 <asaf29999@gmail.com>
2023-12-05 19:39:19 +0200

    add DB connection

--- lib/validations/user.ts
+++ lib/validations/user.ts
@@ -1,6 +1,6 @@
 import * as z from 'zod';
 
-export const userValidation = z.object({
+export const UserValidation = z.object({
     profile_photo: z.string().url().min(1),
     name: z.string().min(3, { message: 'minimum 3 char' }).max(30, { message: 'maximum 30 char' }),
     username: z.string().min(3).max(30),
